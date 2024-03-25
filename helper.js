376f8207a7379b9d8e1fbf05def0b5ea90e353ad
oscaroguledo <122152636+oscaroguledo@users.noreply.github.com>
2024-03-25 21:33:32 +0000

    intro

---
+++ helper.js
@@ -0,0 +1,9 @@
+const people= ['ffgfg','ffgfg','ffgfg'];
+const display=(people)=>{
+    console.log(`name---- ${people}`);
+}
+
+module.exports={
+    display:display,
+    people:people
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+const xyz = require('./helper');
+
+xyz.display('ffgfg');
+console.log(xyz.people);
+const os = require('os');
+console.log(os.hostname(), os.homedir());
+
