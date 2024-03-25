4c44faf9a625040cd7a889c2a37f0ed549583846
oscaroguledo <122152636+oscaroguledo@users.noreply.github.com>
2024-03-25 22:02:58 +0000

    done with filestream

--- filestream.js
+++ filestream.js
@@ -1,4 +1,15 @@
 const fs = require('fs');
 //reading files
 
-const readStream = fs.createReadStream('./large.txt');
+const readStream = fs.createReadStream('./large.txt', {encoding: 'utf-8'});
+const writeStream = fs.createWriteStream('./large2.txt');
+//raw way-------------
+readStream.on('data', (chunk) => {
+    console.log("NEW CHUNK=----");
+    console.log(`${chunk} \n`);
+    writeStream.write('\nNEW CHUNK\n');
+    writeStream.write(chunk);
+});
+
+//using pipe-----------------
+readStream.pipe(writeStream);
